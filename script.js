f51c69ade1ee25e3c1449a1187e336334131743b
DESKTOP-8F9PJFG\AL-JWHARA <monthersaleh366@gmail.com>
2022-01-28 10:13:09 +0200

    improving the code

--- script.js
+++ script.js
@@ -5,31 +5,28 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const navArray = [nav1,nav2,nav3,nav4,nav5];
+
+function slide (from ,to) {
+    navArray.forEach((nav,index)=>{
+        nav.classList.replace(`slide-${from}-${index+1}`,`slide-${to}-${index+1}`)
+    });
+}
 
 function toggleNav () {
     menuBars.classList.toggle('change');
     overlay.classList.toggle('overlay-active');
     if (overlay.classList.contains('overlay-active')) {
         overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
-        nav1.classList.replace('slide-out-1','slide-in-1');
-        nav2.classList.replace('slide-out-2','slide-in-2');
-        nav3.classList.replace('slide-out-3','slide-in-3');
-        nav4.classList.replace('slide-out-4','slide-in-4');
-        nav5.classList.replace('slide-out-5','slide-in-5');
+        slide('out','in');
     }else {
         overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
-        nav1.classList.replace('slide-in-1','slide-out-1');
-        nav2.classList.replace('slide-in-2','slide-out-2');
-        nav3.classList.replace('slide-in-3','slide-out-3');
-        nav4.classList.replace('slide-in-4','slide-out-4');
-        nav5.classList.replace('slide-in-5','slide-out-5');
+        slide('in','out');
     }
 }
 
 // event listener 
 menuBars.addEventListener('click',toggleNav);
-nav1.addEventListener('click',toggleNav);
-nav2.addEventListener('click',toggleNav);
-nav3.addEventListener('click',toggleNav);
-nav4.addEventListener('click',toggleNav);
-nav5.addEventListener('click',toggleNav);
+navArray.forEach(nav=>{
+    nav.addEventListener('click',toggleNav);
+});
